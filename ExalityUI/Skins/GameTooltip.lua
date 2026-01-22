c6b45d58d6d8fb0136066cf45bb6cc130ad16c7c
Exality <a.bedeicis@gmail.com>
2026-01-23 01:39:37 +0200

    Style tooltips statusbar

--- ExalityUI/Skins/GameTooltip.lua
+++ ExalityUI/Skins/GameTooltip.lua
@@ -38,6 +38,19 @@ local function SkinTooltip(tooltip)
             tooltip.CompareHeader:SetFrameLevel(1)
         end
     end
+    if (tooltip.StatusBar) then
+        tooltip.StatusBar:SetStatusBarTexture(EXUI.const.textures.frame.statusBar)
+        local sbBackdrop = CreateFrame('Frame', nil, tooltip.StatusBar, 'BackdropTemplate')
+        sbBackdrop:SetAllPoints()
+        sbBackdrop:SetFrameLevel(0)
+        sbBackdrop:SetBackdrop(EXUI.const.backdrop.pixelPerfect())
+        sbBackdrop:SetBackdropColor(0, 0, 0, 0.4)
+        sbBackdrop:SetBackdropBorderColor(0, 0, 0, 1)
+        if (tooltip.StatusBar:GetFrameLevel() == 0) then
+            tooltip.StatusBar:SetFrameLevel(1)
+        end
+        tooltip.StatusBar.bg = sbBackdrop
+    end
 end
 
 gameTooltipSkin.Init = function(self)
